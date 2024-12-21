acd3e836db22676afc044d3888b9a7da9a2d2dde
Afaque Ahmed <145118453+Afaque-A@users.noreply.github.com>
2024-12-21 17:33:57 +0500

    class created

---
+++ lib/quotes_of_dat.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuotesOfDat extends StatefulWidget {
+  const QuotesOfDat({super.key});
+
+  @override
+  State<QuotesOfDat> createState() => _QuotesOfDatState();
+}
+
+class _QuotesOfDatState extends State<QuotesOfDat> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
